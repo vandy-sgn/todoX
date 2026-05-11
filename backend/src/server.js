74dcedd9548886afb9c2e850f406f1564cbe70e6
Vy Vy <vyvy.tester@gmail.com>
2026-05-11 14:26:34 +0700

    prepare for  deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,21 +12,22 @@ const __dirname = path.resolve();
 
 const app = express();
 
-
-//Middlewares
+//========= Midlewares =========
 app.use(express.json());
 if (process.env.NODE_ENV === "development") {
     app.use(cors({ origin: "http://localhost:5173" }));
 }
+
+//========= API ============
 app.use("/api/tasks", taskRoute);
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
-    app.get("*", (req, res) => { req.sendFile(path.join(__dirname, "../frontend/dist/index.html")) });
+    app.get("*", (req, res) => { res.sendFile(path.join(__dirname, "../frontend/dist/index.html")) });
 }
 
 
-//Connect Mongo Database
+//======== Connect Mongo Database
 connectDB().then(() => {
     app.listen(PORT, () => { console.log(`Chào mừng đến với bình nguyên vô tận - port ${PORT}`) });
 });
