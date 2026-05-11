fe2a86b86e295d6ec3d0987ddfaacf4fcd17b1df
Vy Vy <vyvy.tester@gmail.com>
2026-05-11 13:56:43 +0700

    prepare for  deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,32 @@
 import express from 'express';
-import router from './routes/taskRouters.js'
+import taskRoute from './routes/taskRouters.js'
 import { connectDB } from './config/db.js'
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000
+const __dirname = path.resolve();
 
 const app = express();
 
 
 //Middlewares
 app.use(express.json());
-app.use(cors({origin: "http://localhost:6969"}))
+if (process.env.NODE_ENV === "development") {
+    app.use(cors({ origin: "http://localhost:5173" }));
+}
+app.use("/api/tasks", taskRoute);
 
-app.use("/api/tasks", router);
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => { req.sendFile(path.join(__dirname, "../frontend/dist/index.html")) });
+}
 
+
+//Connect Mongo Database
 connectDB().then(() => {
     app.listen(PORT, () => { console.log(`Chào mừng đến với bình nguyên vô tận - port ${PORT}`) });
 });
